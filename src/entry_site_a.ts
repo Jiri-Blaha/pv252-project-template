33c45854c22ebff36206fa17161117ae8e7fbdaa
Jiří Bláha <jiri.blaha@firma.seznam.cz>
2024-09-23 13:47:18 +0200

    break formatting

--- src/entry_site_a.ts
+++ src/entry_site_a.ts
@@ -1,5 +1,7 @@
 import { initFactorialUi } from "./factorial.ts";
 import { initMenu } from "./menu.ts";
 
-initMenu(document.querySelector(".main-menu")!, "menu-site-a");
+initMenu
+(
+  document.querySelector(".main-menu")!, "menu-site-a");
 initFactorialUi(document.querySelector("#site-a"));
