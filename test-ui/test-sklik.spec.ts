4bce48b01d89922b9a946b424ea3a6f5e4d8a51d
Jiří Bláha <jiri.blaha@firma.seznam.cz>
2024-10-06 19:14:49 +0200

    add tests for www.sklik.cz

---
+++ test-ui/test-sklik.spec.ts
@@ -0,0 +1,39 @@
+import { expect } from "@playwright/test";
+import { test } from "./coverage_wrapper";
+
+
+test("login", async ({ page }) => {
+  await page.goto("https://www.sklik.cz", {timeout : 3000});
+  const logo = page.locator(".login");
+  await expect(logo).toBeVisible();
+});
+
+test("help", async ({ page }) => {
+  await page.goto("https://www.sklik.cz", {timeout : 3000});
+  const help = page.locator("#nav > a").getByText("Nápověda");
+  await help.click();
+  await expect(page).toHaveURL("https://napoveda.sklik.cz/");
+  const search = page.locator("#s");
+  await expect(search).toBeVisible();
+});
+
+test("partner", async ({ page }) => {
+  await page.goto("https://www.sklik.cz", {timeout : 3000});
+  const help = page.locator("#nav > a").getByText("Partner");
+  await help.click();
+  await expect(page).toHaveURL("https://partner.seznam.cz/");
+  const switcher = page.locator(".search-switcher");
+  await switcher.click();
+  const search = page.locator(".header__search");
+  await expect(search).toBeInViewport();
+});
+
+test("faq", async ({ page }) => {
+  await page.goto("https://www.sklik.cz", {timeout : 3000});
+  const help = page.locator("#nav > a").getByText("Nápověda");
+  await help.click();
+  await expect(page).toHaveURL("https://napoveda.sklik.cz/");
+  const faq = page.locator("#menu-hlavni-menu").getByText("Nejčastější dotazy");
+  await faq.click();
+  await expect(page).toHaveURL("https://napoveda.sklik.cz/faq/");
+});
